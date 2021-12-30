12841fdd8b8b63d148284e4bfa911a5f431cfe1c
Ilya Shimbuev <iliyarock@gmail.com>
2021-12-30 19:46:10 +0300

    -Реализованы базовые игровые объекты

---
+++ MyForestGame/Core/BaseObjects/DynamicGameObjectBase.cs
@@ -0,0 +1,30 @@
+﻿using Game.Core.Models;
+using Game.Core.Interfaces;
+
+namespace Game.Core.BaseObjects
+{
+    public class DynamicGameObjectBase : GameObjectBase
+    {
+        public IMovementModule Move { get; set; }
+        public PositionModel PastPosition { get; set; }
+
+        public DynamicGameObjectBase(PositionModel currentPosition)
+        {
+            CurrentPosition = new(currentPosition);
+            PastPosition = new(currentPosition);
+        }
+
+        public void SetMoveModule(MovementModule movementModule)
+            => Move = movementModule;
+
+        public bool CurrentAndPastPositionIsEqual
+            => CurrentPosition.Width == PastPosition.Width && CurrentPosition.Height == PastPosition.Height;
+
+        public void SetPastPosition(int width, int hight)
+            => PastPosition = new(width, hight);
+
+        public void SetPastPosition(PositionModel position)
+            => SetPastPosition(position.Width, position.Height);
+
+    }
+}
---
+++ MyForestGame/Core/BaseObjects/StaticGameObjectBase.cs
@@ -0,0 +1,12 @@
+﻿using Game.Core.Models;
+
+namespace Game.Core.BaseObjects
+{
+    public class StaticGameObjectBase : GameObjectBase
+    {
+        public StaticGameObjectBase(PositionModel currentPosition)
+        {
+            CurrentPosition = new(currentPosition);
+        }
+    }
+}
