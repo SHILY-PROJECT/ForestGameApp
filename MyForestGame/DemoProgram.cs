ded6cc46a5a841ae896b7ce2a4d8cf3bd07b8df6
Ilya Shimbuev <iliyarock@gmail.com>
2021-12-30 19:47:30 +0300

    -Демо

---
+++ MyForestGame/DemoProgram.cs
@@ -0,0 +1,11 @@
+﻿using Game.Core.BaseObjects;
+using MyForestGame.Core;
+
+namespace MyForestGame
+{
+    internal class DemoProgram
+    {
+        internal static void Main()
+            => new MainGame(new GameManager()).Start();
+    }
+}
