a1e2ec6fcb09bd54bd1cdc910ced544b09165885
Ilya Shimbuev <iliyarock@gmail.com>
2022-04-20 20:17:04 +0300

    -Добавлен интерфейс глобального игрового движка -Реализован глобальный игровой движок

---
+++ src/Core/Engines/GameEngine.cs
@@ -0,0 +1,47 @@
+﻿using MyForestGame.Core.Interfaces.Services;
+using System.Threading.Tasks;
+
+namespace MyForestGame.Core.Engines;
+
+public class GameEngine : IGlobalGameEngineService
+{
+    public IRenderEngineService RenderEngine { get; init;}
+    public IWorldEngineService WorldEngine { get; init; }
+    public IPlayerControlService PlayerControl { get; init; }
+    public IGameManagerService GameManager { get; init; }
+
+    public GameEngine(
+        IGameManagerService gameManager,
+        IWorldEngineService worldEngine,
+        IRenderEngineService renderEngine,
+        IPlayerControlService playerControl)
+    {
+        GameManager = gameManager;
+        WorldEngine = worldEngine;
+        RenderEngine = renderEngine;
+        PlayerControl = playerControl;
+    }
+
+    public void Connect()
+    {
+        RenderEngine.Connect();
+        PlayerControl.Connect();
+        Parallel.Invoke(() => this.Update());
+    }
+
+    private void Update()
+    {
+        Task.Run(() =>
+        {
+            while (true)
+            {
+                WorldEngine.UpdateStateWorld();
+                RenderEngine.UpdateRender();
+
+                if (GameManager.IsGameOver) return;
+
+                Task.Delay(10).Wait();
+            }
+        });
+    }
+}
---
+++ src/Core/Interfaces/Services/IGlobalGameEngineService.cs
@@ -0,0 +1,9 @@
+﻿namespace MyForestGame.Core.Interfaces.Services;
+
+public interface IGlobalGameEngineService : IService
+{
+    IRenderEngineService RenderEngine { get; }
+    IWorldEngineService WorldEngine { get; }
+    IPlayerControlService PlayerControl { get; }
+    IGameManagerService GameManager { get; }
+}
