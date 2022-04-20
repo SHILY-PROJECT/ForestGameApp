24f3a259be4a3481d9fc99ff4c96e0cfa3d2f9f8
Ilya Shimbuev <iliyarock@gmail.com>
2022-04-20 20:12:39 +0300

    -Запуск игры переработан

---
+++ src/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using MyForestGame.Core;
+using MyForestGame.Core.Interfaces.Services;
+
+namespace MyForestGame;
+
+internal class Startup
+{
+    public Startup(IMainGameService mainGameService) => Game = mainGameService;
+
+    private IMainGameService Game { get; set; }
+
+    private void StartGame() => Game.Run();
+
+    internal static void Main(string[] args) =>   
+        CreateHostBuilder(args).Build().Services.GetRequiredService<Startup>().StartGame();
+
+    private static IHostBuilder CreateHostBuilder(string[] args) => Host
+        .CreateDefaultBuilder(args)
+        .ConfigureServices(services => services.AddGameComponents());
+}
