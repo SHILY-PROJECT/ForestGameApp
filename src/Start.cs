061c7a25c42222e147ce123654aeb8d25f199884
Ilya Shimbuev <iliyarock@gmail.com>
2022-04-20 18:12:50 +0300

    -Переработан старт игры -Приложение теперь будет работать используя DI

--- src/Start.cs
+++ src/Start.cs
@@ -1,11 +1,48 @@
-﻿using MyForestGame.Core.BaseObjects;
+﻿using System;
+using System.Text;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using MyForestGame.Core;
+using MyForestGame.Core.Interfaces.Services;
+using MyForestGame.Core.BaseObjects;
+using MyForestGame.Core.Engines;
 
-namespace MyForestGame
+namespace MyForestGame;
+
+internal class Start
 {
-    internal class Start
+    public Start(IGameService gameService)
     {
-        internal static void Main()
-            => new MainGame(new GameManager()).Start();
+        Game = gameService;
+    }
+
+    private IGameService Game { get; set; }
+
+    private void StartGame() => Game.Run();
+
+    internal static void Main(string[] args)
+    {
+        SetConsoleSettings();
+        var host = CreateHostBuilder(args).Build();
+        host.Services.GetRequiredService<Start>().StartGame();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) => Host
+        .CreateDefaultBuilder(args)
+        .ConfigureServices(services =>
+        {
+            services
+                .AddSingleton<IGameService, MainGame>()
+                .AddSingleton<IGameManagerService, GameManager>()
+                .AddSingleton<IRenderEngineService, RenderEngine>()
+                .AddSingleton<IWorldEngineService, WorldEngine>();
+        });
+
+    private static void SetConsoleSettings()
+    {
+        Console.InputEncoding = Encoding.UTF8;
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.SetWindowSize(120, 40);
+        Console.CursorVisible = false;
     }
 }
