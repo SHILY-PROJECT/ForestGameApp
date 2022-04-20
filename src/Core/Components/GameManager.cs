7de16af40a1241f09c738c853a53a8372d7db15d
Ilya Shimbuev <iliyarock@gmail.com>
2022-04-20 20:11:35 +0300

    -GameManager переработан

--- src/Core/Components/GameManager.cs
+++ src/Core/Components/GameManager.cs
@@ -1,31 +1,25 @@
 ﻿using System.Collections.Generic;
 using MyForestGame.Core.Models;
 using MyForestGame.Core.Interfaces;
-using MyForestGame.Core.Engines;
 using MyForestGame.Core.GameObjects;
-using MyForestGame.Core.Interfaces;
+using MyForestGame.Core.Interfaces.Services;
 
-namespace MyForestGame.Core.BaseObjects
+namespace MyForestGame.Core.BaseObjects;
+
+public class GameManager : IGameManagerService
 {
-    public class GameManager : IGameManager
-    {
-        public IWorldEngine WorldEngine { get; init; }
-        public IRenderEngine RenderEngine { get; init; }
-        public ICollisionHandler CollisionHandler { get; init; }
-        public GameCounterModel GameCounter { get; set; }
-        public PlayerObject Player { get; set; }
-        public List<IGameObject> GameObjectsСollection { get; set; }
-        public GameObjectsSettingsModel GameObjectsSettings { get; set; }
-        public GameGridSizeModel GridSize { get; set; }
-        public bool IsGameOver { get; set; }       
+    public ICollisionHandler CollisionHandler { get; set; }
+    public GameCounterModel GameCounter { get; set; }
+    public PlayerObject Player { get; set; }
+    public List<IGameObject> GameObjectsСollection { get; set; }
+    public GameObjectsSettingsModel GameObjectsSettings { get; set; }
+    public GameGridSizeModel GridSize { get; set; }
+    public bool IsGameOver { get; set; }       
 
-        public GameManager()
-        {
-            GridSize = new GameGridSizeModel(25, 15);
-            CollisionHandler = new CollisionHandler(this);
-            Player = new PlayerObject(new MovementModule(CollisionHandler, GridSize), new PositionModel(0, 0), "Player");
-            WorldEngine = new WorldEngine(this);
-            RenderEngine = new RenderEngine(this);
-        }
+    public void Connect()
+    {
+        GridSize = new GameGridSizeModel(25, 15);
+        CollisionHandler = new CollisionHandler(this);
+        Player = new PlayerObject(new MovementModule(CollisionHandler, GridSize), new PositionModel(0, 0), "Player");
     }
 }
---
+++ src/Core/Interfaces/Services/IGameManagerService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using MyForestGame.Core.Models;
+using MyForestGame.Core.BaseObjects;
+using MyForestGame.Core.GameObjects;
+
+namespace MyForestGame.Core.Interfaces.Services;
+
+public interface IGameManagerService : IService
+{
+    ICollisionHandler CollisionHandler { get; set; }
+    GameCounterModel GameCounter { get; set; }
+    PlayerObject Player { get; set; }
+    List<IGameObject> GameObjectsСollection { get; set; }
+    GameObjectsSettingsModel GameObjectsSettings { get; set; }
+    GameGridSizeModel GridSize { get; set; }
+
+    bool IsGameOver { get; set; }
+    public static int CurrentTime => (int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+}
