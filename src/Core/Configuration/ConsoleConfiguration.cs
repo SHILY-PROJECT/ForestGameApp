e86a7daa4ca1f12b2eae0fae99e2b00751735451
Ilya Shimbuev <iliyarock@gmail.com>
2022-04-29 18:22:14 +0300

    -Настройка размера консоли вынесена в отдельный класс

---
+++ src/Core/Configuration/ConsoleConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace ForestGame.Core.Configuration;
+
+internal class ConsoleConfiguration
+{
+    public static void Set(IGameGridSize gridSize)
+    {       
+        Console.InputEncoding = Encoding.UTF8;
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.SetWindowSize((int)(gridSize.Width * 4.1), (int)(gridSize.Height * 2.22));
+        Console.CursorVisible = false;
+    }
+}
--- src/Core/Configuration/GameComponentRegistrar.cs
+++ src/Core/Configuration/GameComponentRegistrar.cs
@@ -4,8 +4,6 @@ internal static class GameComponentRegistrar
 {
     public static IServiceCollection AddGameComponents(this IServiceCollection services)
     {
-        SetConsoleSettings();
-
         services
             .AddSingleton<Startup>()
             .AddSingleton<Player>()
@@ -26,7 +24,9 @@ internal static class GameComponentRegistrar
 
     private static GameGridSize GetGridSize()
     {
-        return new GameGridSize(25, 15);
+        var gridSize = new GameGridSize(35, 20);
+        ConsoleConfiguration.Set(gridSize);
+        return gridSize;
     }
 
     private static PlayerSettings GetPlayerSettings()
@@ -39,13 +39,5 @@ internal static class GameComponentRegistrar
             ColorBackground = ConsoleColor.DarkBlue,
             StartPosition = new(0, 0)
         };
-    }
-
-    private static void SetConsoleSettings()
-    {
-        Console.InputEncoding = Encoding.UTF8;
-        Console.OutputEncoding = Encoding.UTF8;
-        Console.SetWindowSize(120, 40);
-        Console.CursorVisible = false;
     }
 }
